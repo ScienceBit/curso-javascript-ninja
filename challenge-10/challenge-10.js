80e18ac256c7e8564fccc02c9fa8d24855f25e24
Marcos Inocencio <45172643+ScienceBit@users.noreply.github.com>
2020-07-27 03:20:42 -0300

    Update challenge-10.js

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -62,12 +62,12 @@
   */
   function calculator(oper)
   {
-   if(!isOperatorValid(oper)){ return false; }
+    if(!isOperatorValid(oper)){ return false; }
 
-   return function(x, y)
-   {
-     return typeof x !== 'number' && typeof y !== 'number' ? false : operation[oper](x, y);
-   }
+    return function(x, y)
+    {
+      return typeof x !== 'number' && typeof y !== 'number' ? false : operation[oper](x, y);
+    }
   }
 
   /*
@@ -124,13 +124,13 @@
   */
   if(sum)
   {
-   number1 = 10;
-   number2 = 2;
-   console.log(showOperationMessage(operationSignal, number1, number2), sum(number1, number2));
+     number1 = 10;
+     number2 = 2;
+     console.log(showOperationMessage(operationSignal, number1, number2), sum(number1, number2));
   }
   else
   {
-   console.log(showErrorMenssage(operationSignal));
+     console.log(showErrorMenssage(operationSignal));
   }
 
   /*
@@ -143,13 +143,13 @@
 
   if(subtraction)
   {
-   number1 = 10;
-   number2 = 2;
-   console.log(showOperationMessage(operationSignal, number1, number2), subtraction(number1, number2));
+     number1 = 10;
+     number2 = 2;
+     console.log(showOperationMessage(operationSignal, number1, number2), subtraction(number1, number2));
   }
   else
   {
-   console.log(showErrorMessage(operationSignal));
+     console.log(showErrorMessage(operationSignal));
   }
 
   operationSignal = '*';
@@ -157,13 +157,13 @@
 
   if(multiplication)
   {
-   number1 = 10;
-   number2 = 2;
-   console.log(showOperationMessage(operationSignal, number1, number2), multiplication(number1, number2));
+     number1 = 10;
+     number2 = 2;
+     console.log(showOperationMessage(operationSignal, number1, number2), multiplication(number1, number2));
   }
   else
   {
-   console.log(showErrorMessage(operationSignal));
+     console.log(showErrorMessage(operationSignal));
   }
 
   operationSignal = '/';
@@ -171,13 +171,13 @@
 
   if(division)
   {
-   number1 = 10;
-   number2 = 2;
-   console.log(showOperationMessage(operationSignal, number1, number2), division(number1, number2));
+     number1 = 10;
+     number2 = 2;
+     console.log(showOperationMessage(operationSignal, number1, number2), division(number1, number2));
   }
   else
   {
-   console.log(showErrorMenssage(operationSignal));
+     console.log(showErrorMenssage(operationSignal));
   }
 
   operationSignal = '%';
@@ -185,13 +185,13 @@
 
   if(mod)
   {
-   number1 = 10;
-   number2 = 2;
-   console.log(showOperationMessage(operationSignal, number1, number2), mod(number1, number2));
+     number1 = 10;
+     number2 = 2;
+     console.log(showOperationMessage(operationSignal, number1, number2), mod(number1, number2));
   }
   else
   {
-   console.log(showErrorMessage(operationSignal));
+     console.log(showErrorMessage(operationSignal));
   }
 
   /*
@@ -203,7 +203,7 @@
 
   if(!invalid)
   {
-    console.log(showErrorMessage(operationSignal));
+     console.log(showErrorMessage(operationSignal));
   }
 
 }());
